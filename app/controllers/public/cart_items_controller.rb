282c88b310a7a81dd1780ebd4435d41972f16234
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-06-11 07:27:15 +0000

    カート機能追加中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,10 +1,22 @@
 class Public::CartItemsController < ApplicationController
   def index
   end
-  
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def destroy_all
+  end
+
+  def create
+  end
+
   private
   def cart_item_params
       params.require(:cart_item).permit(:item_id, :amount)
   end
-  
+
 end

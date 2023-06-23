36b9f31db1d75c70c83df1803b745e7a8d8e49ea
dolphinderby <129580551+dolphinderby@users.noreply.github.com>
2023-06-23 10:08:45 +0000

    3.8実装中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -22,7 +22,7 @@ class Public::OrdersController < ApplicationController
       order_item.item_id = cart.item_id
       order_item.order_id = @order.id
       order_item.amount = cart.amount
-      order_item.purchase_price = cart.item.price
+      order_item.purchase_price = order.payment_method
       order_item.save
     end
     redirect_to orders_complete_path
